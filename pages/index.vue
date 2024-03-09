5faecd859c7c1c358d3a45da4d161b7bc548764d
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-03-10 00:37:15 +0100

    feat: wip starting out

--- pages/index.vue
+++ pages/index.vue
@@ -8,33 +8,5 @@ useSeoMeta({
 </script>
 
 <template>
-  <div class="relative h-screen w-screen">
-    <NuxtImg class="absolute inset-0 h-full w-full bg-no-repeat bg-cover" src="/images/background.jpg" />
-    <main class="absolute inset-0 flex items-center justify-center">
-      <Card class="w-[400px] h-[225px]">
-        <CardHeader>
-          <CardTitle>🚀 in development</CardTitle>
-          <CardDescription>Any questions?</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p>
-            <Icon name="i-material-symbols-alternate-email-rounded"/>
-            dinokupinic2006@gmail.com
-          </p>
-          <p>
-            <Icon name="i-skill-icons-discord"/>
-            yuzzuwo
-          </p>
-        </CardContent>
-        <CardFooter>
-          <p>
-            In the meantime, you can checkout my
-            <NuxtLink to="https://github.com/Dino-Kupinic">
-              <Icon name="i-mdi-github"/>
-            </NuxtLink>
-          </p>
-        </CardFooter>
-      </Card>
-    </main>
-  </div>
+
 </template>
