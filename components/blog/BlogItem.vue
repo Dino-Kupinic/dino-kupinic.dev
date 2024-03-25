f93614b28231c3d5050299368fc02e4ddecc84bf
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-03-26 00:00:54 +0100

    feat(wip): rework blog cards

--- components/blog/BlogItem.vue
+++ components/blog/BlogItem.vue
@@ -6,32 +6,40 @@ defineProps<{
   date: Date
   author: string
 }>()
+const img = useImage()
 </script>
 
 <template>
-  <Card class="flex h-[300px] flex-col sm:h-auto">
-    <CardHeader
-      class="flex-auto items-center justify-center object-cover p-2 pb-1 sm:p-3 sm:pb-2"
-    >
-      <BlogItemHeader />
-    </CardHeader>
-    <CardContent class="flex-none px-3 py-0 sm:px-3">
-      <BlogItemTitle>
-        <p class="line-clamp-2">
-          {{ title }}
-        </p>
-      </BlogItemTitle>
-      <div class="mt-1 flex space-x-3">
-        <BlogLikes>{{ likes }}</BlogLikes>
-        <BlogViews>{{ views }}</BlogViews>
-      </div>
-    </CardContent>
-    <CardFooter class="mt-4 p-2 pt-1 sm:p-3 sm:pt-1.5">
-      <div class="flex w-full justify-between space-x-3">
-        <BlogItemAuthor>{{ author }}</BlogItemAuthor>
-        <BlogItemDate>{{ date.toLocaleDateString() }}</BlogItemDate>
-      </div>
-    </CardFooter>
+  <Card
+    class="relative flex h-[220px] flex-col overflow-hidden after:absolute after:left-0 after:top-0 after:h-full after:w-full after:bg-gradient-to-t after:from-black after:to-transparent after:transition-opacity after:duration-200 after:content-[''] sm:h-auto"
+  >
+    <NuxtImg
+      class="absolute left-0 top-0 h-full w-full overflow-clip object-cover object-top"
+      src="/images/gradientbg.jpg"
+      alt="blog-background"
+      :placeholder="
+        img(`/images/gradientbg.jpg`, { h: 10, f: 'png', blur: 2, q: 50 })
+      "
+    />
+    <div class="z-10 flex h-full w-full flex-col justify-end p-4">
+      <CardContent class="flex-none p-0">
+        <BlogItemTitle>
+          <p class="line-clamp-2">
+            {{ title }}
+          </p>
+        </BlogItemTitle>
+        <div class="mt-1 flex space-x-3">
+          <BlogLikes>{{ likes }}</BlogLikes>
+          <BlogViews>{{ views }}</BlogViews>
+        </div>
+      </CardContent>
+      <CardFooter class="mt-4 p-0">
+        <div class="flex w-full justify-between space-x-3">
+          <BlogItemAuthor>{{ author }}</BlogItemAuthor>
+          <BlogItemDate>{{ date.toLocaleDateString() }}</BlogItemDate>
+        </div>
+      </CardFooter>
+    </div>
   </Card>
 </template>
 
---
+++ components/blog/BlogItemBackground.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const img = useImage()
+</script>
+
+<template>
+  <NuxtImg
+    class="h-full w-full object-cover"
+    src="/images/gradientbg.jpg"
+    alt="blog-background"
+    :placeholder="
+      img(`/images/gradientbg.jpg`, { h: 10, f: 'png', blur: 2, q: 50 })
+    "
+  />
+</template>
+
+<style scoped></style>
