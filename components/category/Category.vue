fc61c99dcfca1792b4fcac3bf7c790b1b797dbe5
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-04-07 20:54:27 +0200

    feat: rename

--- components/category/Category.vue
+++ components/category/Category.vue
@@ -1,5 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineProps<{
+  text: string
+  to: string
+}>()
+</script>
 
-<template></template>
+<template>
+  <div>
+    <NuxtLink :to>
+      <Button variant="gradient" size="xs" class="rounded-full">
+        <span class="mx-1.5 text-sm font-normal">
+          {{ text }}
+        </span>
+      </Button>
+    </NuxtLink>
+  </div>
+</template>
 
 <style scoped></style>
