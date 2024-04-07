a367df92df4241d67739be8d8a8285429430759b
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-04-07 20:53:03 +0200

    feat: small improvements

--- components/ui/button/index.ts
+++ components/ui/button/index.ts
@@ -16,7 +16,7 @@ export const buttonVariants = cva(
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
-        gradient: 'bg-primary dark:bg-gradient-to-t from-foreground to-secondary text-primary-foreground',
+        gradient: 'bg-primary dark:bg-gradient-to-t from-foreground from-30% to-secondary text-primary-foreground',
       },
       size: {
         default: 'h-10 px-4 py-2',
--- types/blog.ts
+++ types/blog.ts
@@ -6,6 +6,7 @@ export type Blog = z.infer<typeof selectBlogSchema>
 export interface BlogAuthor {
   name: string
   avatar: string
+  handle: string
 }
 
 export interface BlogContent extends ParsedContent {
