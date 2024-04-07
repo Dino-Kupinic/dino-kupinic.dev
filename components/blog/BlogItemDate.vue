2bda8b2e32898033efa014ce1a2cb388a4efaec3
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-04-07 20:54:19 +0200

    feat: added new blog components

--- components/blog/BlogItemDate.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <span class="flex items-center text-sm text-primary dark:text-secondary">
-    <slot />
-  </span>
-</template>
---
+++ components/sections/blog/BlogContent.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex flex-col sm:flex-row">
+    <div class="sm:border-r sm:pr-12">
+      <slot name="main" />
+    </div>
+    <SideBarContainer>
+      <slot name="sidebar" />
+    </SideBarContainer>
+  </div>
+</template>
