c5bb726d0eae022084887de8d1ae12956103368e
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-04-08 19:52:56 +0200

    feat(wip): Table of contents work in progress

--- components/blog/BlogTableOfContents.vue
+++ components/blog/BlogTableOfContents.vue
@@ -1,10 +1,41 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { TocLink } from "@nuxt/content/types"
+
+defineProps<{
+  links: TocLink[]
+}>()
+
+const route = useRoute()
+</script>
 
 <template>
-  <div>
-    <span>Table of Contents</span>
-    <ul>
-      <li v-for="x in 5" :key="x">{{ x }} point</li>
+  <div class="pb-6">
+    <p class="text-lg font-semibold">Table of Contents</p>
+    <ul class="mt-3">
+      <NuxtLink
+        v-for="link in links"
+        :key="link.id"
+        :to="route.path + '#' + link.id"
+      >
+        <li
+          class="truncate text-sm leading-6 text-neutral-500 hover:text-foreground dark:text-neutral-500 dark:hover:text-foreground"
+        >
+          <span>
+            {{ link.text }}
+          </span>
+          <!--          <NuxtLink-->
+          <!--            v-for="l in link.children"-->
+          <!--            :key="l.id"-->
+          <!--            :to="route.path + '#' + l.id"-->
+          <!--          >-->
+          <!--            <li-->
+          <!--              class="ml-3 truncate text-sm leading-6 text-neutral-500 hover:text-foreground dark:text-neutral-400"-->
+          <!--            >-->
+          <!--              {{ l.text }}-->
+          <!--            </li>-->
+          <!--          </NuxtLink>-->
+        </li>
+      </NuxtLink>
     </ul>
   </div>
 </template>
