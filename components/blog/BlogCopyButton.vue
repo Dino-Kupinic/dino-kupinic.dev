cc1cf4ce70f0033728257c86382ad867474f3ffe
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-04-21 22:10:54 +0200

    feat: small changes

--- components/blog/BlogCopyButton.vue
+++ components/blog/BlogCopyButton.vue
@@ -5,15 +5,16 @@ const props = defineProps<{
   link: string
 }>()
 
-const source = ref<string>(props.link)
-const { copy, copied, isSupported } = useClipboard({ source })
+const source = ref<string>(`https://dino-kupinic.dev${props.link}`)
+const { copy, isSupported } = useClipboard({ source })
 const { toast } = useToast()
+const { t } = useI18n()
 
 function copyToClipboard() {
   copy(source.value)
   toast({
-    title: "Copied to clipboard! 👌🏻",
-    description: `Value: ${source.value}`,
+    title: t("clipboard.copy"),
+    description: source.value,
   })
 }
 </script>
@@ -21,17 +22,16 @@ function copyToClipboard() {
 <template>
   <ClientOnly>
     <div v-if="isSupported">
-      <Button variant="ghost" size="sm" @click="copyToClipboard()">
+      <Button variant="ghost" size="default" @click="copyToClipboard()">
         <span class="text-sm">
           <Icon name="i-ph-link" class="mr-1" />
-          <span v-if="!copied">{{ $t("blog.copy") }}</span>
-          <span v-else>Copied!</span>
+          <span>{{ $t("blog.copy") }}</span>
         </span>
       </Button>
     </div>
-    <p v-else>Your browser does not support Clipboard API</p>
+    <p v-else>{{ $t("copy.unsupported") }}</p>
     <template #fallback>
-      <Skeleton class="h-9 w-24" />
+      <Skeleton class="h-10 w-28" />
     </template>
   </ClientOnly>
 </template>
