ea2fe1770d4cd1507c3bd1346db7cf84e78dd3d7
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-04-27 16:26:52 +0200

    feat: change logo

--- components/nav/NavLogo.vue
+++ components/nav/NavLogo.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { theme } from "#tailwind-config"
 
-const DEFAULT_WIDTH: string = "75px"
+const DEFAULT_WIDTH: string = "78px"
 const DEFAULT_HEIGHT: string = "35px"
 
 withDefaults(
@@ -16,14 +16,12 @@ withDefaults(
 )
 
 const mode = useColorMode()
-const mainColor = ref("")
 const fontColor = ref("")
 
 function updateColors() {
   if (mode.value === "dark") {
-    mainColor.value = fontColor.value = theme.colors.foreground
+    fontColor.value = theme.colors.foreground
   } else {
-    mainColor.value = theme.colors.background
     fontColor.value = "#0A0A0A"
   }
 }
@@ -35,34 +33,32 @@ watch(mode, updateColors, { immediate: true })
   <svg
     :width="width"
     :height="height"
-    viewBox="0 0 75 34"
+    viewBox="0 0 78 35"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <g style="mix-blend-mode: difference">
-      <circle cx="17" cy="17" r="7" class="fill" />
-    </g>
-    <g style="mix-blend-mode: difference">
-      <circle cx="7" cy="7" r="7" class="fill" />
-    </g>
-    <g style="mix-blend-mode: difference">
-      <circle cx="27" cy="27" r="7" class="fill" />
-    </g>
-    <g style="mix-blend-mode: difference">
-      <circle cx="17" cy="17" r="15" class="fill" />
-    </g>
+    <circle cx="17.5" cy="17.5" r="17.5" fill="url(#paint0_linear_181_36)" />
     <path
+      d="M51.584 9.96C57.008 9.96 60.056 13.032 60.056 18.504C60.056 23.952 57.08 27 51.728 27H45.92V9.96H51.584ZM49.04 24.288H51.584C55.136 24.288 56.864 22.392 56.864 18.504C56.864 14.568 55.136 12.672 51.584 12.672H49.04V24.288ZM61.5247 9.96H64.6447V17.736L71.3167 9.96H75.0127L68.6287 17.448L75.4207 27H71.8447L66.5647 19.584L64.6447 21.768V27H61.5247V9.96Z"
       class="font"
-      d="M50.6364,26H44.7216V8.54545H50.7557C52.4886,8.54545 53.9773,8.89489,55.2216,9.59375C56.4716,10.2869,57.4318,11.2841,58.1023,12.5852C58.7727,13.8864,59.108,15.4432,59.108,17.2557C59.108,19.0739,58.7699,20.6364,58.0938,21.9432C57.4233,23.25,56.4545,24.2528,55.1875,24.9517C53.9261,25.6506,52.4091,26,50.6364,26ZM47.8835,23.2642H50.483C51.6989,23.2642 52.7131,23.0426,53.5256,22.5994C54.3381,22.1506,54.9489,21.483,55.358,20.5966C55.767,19.7045,55.9716,18.5909,55.9716,17.2557C55.9716,15.9205,55.767,14.8125,55.358,13.9318C54.9489,13.0455,54.3438,12.3835,53.5426,11.946C52.7472,11.5028,51.7585,11.2812,50.5767,11.2812H47.8835V23.2642ZM59.9522,26V8.54545H63.1141V16.5653H63.3272L70.1369,8.54545H73.9977L67.2477,16.3778L74.0573,26H70.2562L65.0488,18.517L63.1141,20.8011V26H59.9522Z"
     />
+    <defs>
+      <linearGradient
+        id="paint0_linear_181_36"
+        x1="0"
+        y1="18.0645"
+        x2="35"
+        y2="18.0645"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="white" />
+        <stop offset="1" stop-color="#1B1B1B" />
+      </linearGradient>
+    </defs>
   </svg>
 </template>
 
 <style scoped>
-.fill {
-  fill: v-bind(mainColor);
-}
-
 .font {
   fill: v-bind(fontColor);
 }
