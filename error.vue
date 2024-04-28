c8503a7e95093f829af3bf71d982f8ef580858bf
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-04-28 19:36:15 +0200

    feat: improve error page

--- error.vue
+++ error.vue
@@ -4,19 +4,25 @@ import type { NuxtError } from "#app"
 defineProps<{ error: NuxtError }>()
 
 useSeoMeta({
-  title: "Page not found",
-  description: "Sadly this page could not be found.",
+  title: "Error",
+  description: "Oops! An error occured.",
 })
 </script>
 
 <template>
-  <div class="m-3 flex flex-col space-y-2">
-    <SectionHeading>An Error occured ⚠️</SectionHeading>
-    <div class="rounded-lg border bg-background p-3">
-      <pre class="overflow-hidden">{{ error }}</pre>
+  <div class="flex h-screen w-full items-center justify-center">
+    <div class="m-3 flex flex-col space-y-3">
+      <div class="rounded-lg border bg-background p-3">
+        <SectionHeading class="text-center">
+          {{ $t("error.title") }}
+        </SectionHeading>
+      </div>
+      <div class="rounded-lg border bg-background p-3">
+        <pre class="overflow-hidden">{{ error }}</pre>
+      </div>
+      <NuxtLink to="/">
+        <Button class="w-full">{{ $t("error.home") }}</Button>
+      </NuxtLink>
     </div>
-    <NuxtLink to="/">
-      <Button> Go home </Button>
-    </NuxtLink>
   </div>
 </template>
