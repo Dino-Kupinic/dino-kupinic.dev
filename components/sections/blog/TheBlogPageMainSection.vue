08bfe81c9b5c43b913d2e34dd9a245d2faa2ded7
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-04-28 19:36:03 +0200

    feat: blog main section mostly finished

--- components/sections/blog/TheBlogPageMainSection.vue
+++ components/sections/blog/TheBlogPageMainSection.vue
@@ -3,17 +3,44 @@
 <template>
   <div>
     <FeaturedBlogsContainer class="m-auto my-4 sm:my-5" />
-    <main>
-      <ContentList v-slot="{ list }" path="/blogs">
-        <div
-          v-for="article in list"
-          :key="article._path"
-          class="my-2 border p-3"
-        >
-          <h2>{{ article.title }}</h2>
-          <p>{{ article.description }}</p>
-        </div>
-      </ContentList>
+    <main class="my-16 flex flex-col gap-4">
+      <SectionHeading>{{ $t("blog.latest") }}</SectionHeading>
+      <Table>
+        <TableBody>
+          <ContentList v-slot="{ list }" path="/blogs">
+            <TableRow
+              v-for="article in list"
+              :key="article._path"
+              class="w-full p-3 hover:bg-accent"
+            >
+              <TableCell class="w-full">
+                <NuxtLink :to="article._path">
+                  <div class="flex max-h-24 w-full flex-col space-y-4">
+                    <div>
+                      <BlogItemTitle class="line-clamp-1">
+                        {{ article.title }}
+                      </BlogItemTitle>
+                      <p class="line-clamp-1">{{ article.description }}</p>
+                    </div>
+                    <div class="flex justify-between space-x-2">
+                      <BlogItemAuthor
+                        :author="article.authors"
+                        class="truncate"
+                      />
+                      <BlogDate :date="new Date(article.date)" :short="true" />
+                    </div>
+                  </div>
+                </NuxtLink>
+              </TableCell>
+            </TableRow>
+          </ContentList>
+        </TableBody>
+      </Table>
+      <div class="flex w-full">
+        <Button class="w-full px-24 sm:w-auto">
+          {{ $t("blog.loadMore") }}
+        </Button>
+      </div>
     </main>
   </div>
 </template>
