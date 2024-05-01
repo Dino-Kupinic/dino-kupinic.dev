9349ea540010474056134dbe4652e38a0347df99
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-05-01 20:14:57 +0200

    feat: add tooltip to fav framework

--- components/about/AboutFavouriteFrameworkItem.vue
+++ components/about/AboutFavouriteFrameworkItem.vue
@@ -5,13 +5,22 @@
     <template #header>{{ $t("about.framework") }}</template>
     <template #content>
       <div class="flex h-full w-full items-center justify-center">
-        <NuxtLink to="https://nuxt.com" external>
-          <Icon
-            name="i-simple-icons-nuxtdotjs"
-            class="h-32 w-32"
-            aria-label="Nuxt Logo"
-          />
-        </NuxtLink>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <NuxtLink to="https://nuxt.com" external>
+                <Icon
+                  name="i-simple-icons-nuxtdotjs"
+                  class="h-32 w-32"
+                  aria-label="Nuxt Logo"
+                />
+              </NuxtLink>
+            </TooltipTrigger>
+            <TooltipContent side="bottom">
+              <p>Nuxt</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
       </div>
     </template>
   </AboutItem>
