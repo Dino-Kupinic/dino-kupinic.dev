44bfdd17d9ed453dd647f40d586aafbed94aa42a
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-05-25 18:42:24 +0200

    feat: add coming soon

--- components/work/WorkContainer.vue
+++ components/work/WorkContainer.vue
@@ -4,7 +4,15 @@
   <div
     class="flex h-[500px] w-full flex-col gap-5 rounded-lg border bg-background p-5 shadow-sm sm:flex-row"
   >
-    <div class="h-full w-full rounded-lg bg-accent"></div>
-    <div class="h-full w-full rounded-lg bg-accent"></div>
+    <div
+      class="flex h-full w-full items-center justify-center rounded-lg bg-accent"
+    >
+      <p class="font-heading text-xl sm:text-2xl">Coming soon...</p>
+    </div>
+    <div
+      class="flex h-full w-full items-center justify-center rounded-lg bg-accent"
+    >
+      <p class="font-heading text-xl sm:text-2xl">Coming soon...</p>
+    </div>
   </div>
 </template>
