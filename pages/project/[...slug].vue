2fedf7133897a786ceec7e17c09b1ed2830ab512
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-05-27 14:41:23 +0200

    feat: add deployment info and repo info to projects

--- pages/project/[...slug].vue
+++ pages/project/[...slug].vue
@@ -22,6 +22,8 @@ useSeoMeta({
   description: project.value?.description,
   ogDescription: project.value?.description,
 })
+
+const formattedDate = formatDate(new Date(project.value.date))
 </script>
 
 <template>
@@ -32,15 +34,18 @@ useSeoMeta({
       base="/projects"
     >
       <template #breadcrumb>
-        <Icon name="i-ph-lightbulb" class="mr-1" />
+        <Icon name="i-ph-cube-focus" class="mr-1" />
         <span class="text-sm tracking-wide">
           {{ $t("project.name") }}
         </span>
       </template>
     </ContentHeader>
-    <div class="flex flex-col space-x-2 sm:flex-row">
+    <div class="mb-8 flex flex-col space-y-4 sm:flex-row sm:space-x-2">
       <div class="grow-0 basis-3/5">
-        <div class="w-max rounded-lg bg-purple-100 dark:bg-purple-900">
+        <div
+          v-if="isFeatured"
+          class="w-max rounded-lg bg-purple-100 dark:bg-purple-900"
+        >
           <span
             class="mx-1.5 text-xs font-normal text-purple-600 dark:text-purple-100 sm:text-sm"
           >
@@ -60,8 +65,52 @@ useSeoMeta({
         </div>
       </div>
       <div class="basis-2/5">
-        <div class="h-full w-full rounded-lg bg-accent p-8"></div>
+        <div class="h-full w-full space-y-1 rounded-lg bg-accent p-8">
+          <p class="mb-2 font-semibold">Statistics</p>
+          <div class="dark:text-secondary">
+            <Icon name="i-ph-calendar" class="mr-2" />
+            <span>Started {{ formattedDate }}</span>
+          </div>
+          <div v-if="project?.deployed" class="flex items-center">
+            <Icon name="i-heroicons-check-circle" class="mr-2 text-green-500" />
+            <span class="dark:text-secondary">
+              <NuxtLink :to="project.deployed.url" class="hover:text-blue-500">
+                <span>Deployed on</span>
+                <Icon :name="project?.deployed?.icon" class="mx-1 mb-0.5" />
+                <span>{{ project?.deployed?.vendor }}</span>
+                <Icon
+                  name="i-solar-arrow-right-up-linear"
+                  class="mb-0.5 ml-1"
+                />
+              </NuxtLink>
+            </span>
+          </div>
+          <div v-if="project?.repository" class="flex items-center">
+            <Icon name="i-heroicons-check-circle" class="mr-2 text-green-500" />
+            <span class="dark:text-secondary">
+              <NuxtLink
+                :to="project.repository.url"
+                class="hover:text-blue-500"
+              >
+                <span>Repository on Github</span>
+                <Icon
+                  name="i-solar-arrow-right-up-linear"
+                  class="mb-0.5 ml-1"
+                />
+              </NuxtLink>
+            </span>
+          </div>
+        </div>
       </div>
     </div>
+    <main class="w-3/5">
+      <ContentDoc>
+        <template #empty>
+          <div class="h-[400px]">
+            <h1>No further description provided.</h1>
+          </div>
+        </template>
+      </ContentDoc>
+    </main>
   </ContentLayoutWrapper>
 </template>
