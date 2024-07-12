ffb541d7ff510a808a38b82872ee9709f0633992
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-07-12 21:40:21 +0200

    feat: new dir structure

---
+++ app/components/ui/carousel/CarouselPrevious.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ArrowLeft } from "lucide-vue-next"
+import { useCarousel } from "./useCarousel"
+import type { WithClassAsProps } from "./interface"
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+
+const props = defineProps<WithClassAsProps>()
+
+const { orientation, canScrollPrev, scrollPrev } = useCarousel()
+</script>
+
+<template>
+  <Button
+    :disabled="!canScrollPrev"
+    :class="
+      cn(
+        'absolute h-8 w-8 touch-manipulation rounded-full p-0 sm:h-10 sm:w-10',
+        orientation === 'horizontal'
+          ? '-left-4 top-1/2 -translate-y-1/2 sm:-left-6 lg:-left-12 xl:-left-16'
+          : '-top-12 left-1/2 -translate-x-1/2 rotate-90',
+        props.class,
+      )
+    "
+    variant="outline"
+    @click="scrollPrev"
+  >
+    <slot>
+      <ArrowLeft class="h-4 w-4 text-current sm:h-5 sm:w-5" />
+    </slot>
+  </Button>
+</template>
