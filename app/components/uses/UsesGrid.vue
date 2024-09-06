901685258ae02c36dea7c41cb9ed4b255e993860
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-09-06 21:56:15 +0200

    feat: mostly done with devices

--- app/components/uses/UsesGrid.vue
+++ app/components/uses/UsesGrid.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <section class="grid grid-cols-1 gap-3 sm:grid-cols-3 md:grid-cols-4">
+  <section class="grid grid-cols-1 gap-4 sm:grid-cols-3 md:grid-cols-4">
     <slot />
   </section>
 </template>
--- app/components/uses/UsesItemCover.vue
+++ app/components/uses/UsesItemCover.vue
@@ -8,12 +8,14 @@ defineProps<{
 
 <template>
   <Card class="hover:bg-accent">
-    <div class="flex h-full flex-col justify-between gap-3 p-5">
+    <div class="flex h-full flex-col gap-3 p-5">
       <CardHeader class="p-0">
         <CardTitle>{{ title }}</CardTitle>
         <CardDescription>{{ description }}</CardDescription>
       </CardHeader>
-      <div class="flex h-40 w-40 w-full items-center justify-center">
+      <div
+        class="flex h-64 w-64 w-full grow items-center justify-center bg-gradient-to-t from-background to-accent dark:to-muted"
+      >
         <NuxtImg :src="image" class="max-h-full max-w-full object-contain" />
       </div>
     </div>
