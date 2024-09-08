872704533e71996297503dfb4590f4bab578352a
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-09-08 14:37:25 +0200

    feat: work on apps

--- app/components/uses/UsesItemCover.vue
+++ app/components/uses/UsesItemCover.vue
@@ -7,14 +7,14 @@ defineProps<{
 </script>
 
 <template>
-  <Card class="hover:bg-accent">
+  <Card>
     <div class="flex h-full flex-col gap-3 p-5">
       <CardHeader class="p-0">
-        <CardTitle>{{ title }}</CardTitle>
+        <CardTitle class="tracking-tighter">{{ title }}</CardTitle>
         <CardDescription>{{ description }}</CardDescription>
       </CardHeader>
       <div
-        class="flex h-64 w-64 w-full grow items-center justify-center bg-gradient-to-t from-background to-accent dark:to-muted"
+        class="flex h-64 w-full grow items-center justify-center bg-gradient-to-t from-background to-accent dark:to-muted"
       >
         <NuxtImg :src="image" class="max-h-full max-w-full object-contain" />
       </div>
