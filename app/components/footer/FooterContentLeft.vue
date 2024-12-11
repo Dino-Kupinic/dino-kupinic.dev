139e780f97ef47cab91adcbb683c5125a6e30c99
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-12-11 17:58:34 +0100

    feat: add sidebox project

--- app/components/footer/FooterContentLeft.vue
+++ app/components/footer/FooterContentLeft.vue
@@ -8,10 +8,21 @@
       <p class="font-semibold">Friend space</p>
       <p class="text-primary dark:text-secondary">Cool projects from friends</p>
     </div>
+    <NuxtLink to="https://sidebox.net" target="_blank" rel="noopener">
+      <div
+        class="flex items-center justify-between rounded-lg bg-gradient-to-r from-amber-200 to-yellow-400 p-2 px-3 text-black"
+      >
+        <p>Sidebox</p>
+        <Icon name="i-heroicons-arrow-up-right" />
+      </div>
+    </NuxtLink>
     <div
       class="flex h-full items-center justify-center bg-neutral-100 p-3 dark:bg-neutral-900 sm:p-0"
     >
-      <p>Empty! aww...</p>
+      <p>
+        More on the way! <br />
+        Hopefully...
+      </p>
     </div>
   </div>
 </template>
