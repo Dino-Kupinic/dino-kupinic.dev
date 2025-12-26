d159574488ddaafeb149096bc8baab8f7cf9e3f5
Dino-Kupinic <dinokupinic2006@gmail.com>
2025-12-26 11:14:52 +0100

    feat: enhance Nuxt.js configuration with SEO and linting support

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,15 @@
+<script setup lang="ts">
+useSeoMeta({
+  ogTitle: "Discover Dino Kupinic's Portfolio",
+  description: "Personal portfolio website",
+  ogDescription:
+    "Dino Kupinic is a personal portfolio website designed to showcase the professional work and " +
+    "personal information of Dino Kupinic.",
+});
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,15 +2,24 @@
 import tailwindcss from "@tailwindcss/vite"
 
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  css: ['~/assets/css/tailwind.css'],
+  css: ["~/assets/css/tailwind.css"],
   vite: {
-    plugins: [
-      tailwindcss(),
-    ],
+    plugins: [tailwindcss()],
   },
-  modules: ['shadcn-nuxt'],
+  modules: [
+    "shadcn-nuxt",
+    "@nuxt/eslint",
+    "@nuxtjs/seo",
+    "@nuxt/content",
+    "@nuxt/image",
+    "@nuxtjs/color-mode",
+    "@nuxt/fonts",
+    "@nuxt/scripts",
+    "@vueuse/nuxt",
+    "@nuxt/icon",
+  ],
   shadcn: {
     prefix: "",
     componentDir: "./app/components/ui",
