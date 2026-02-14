784aa2eaeddf02ffcc3fac3081f1ceb03fd28cc6
Dino-Kupinic <dinokupinic2006@gmail.com>
2026-02-14 10:42:12 +0100

    feat: enhance blog sidebar for mobile with improved navigation links

--- app/components/blog/BlogTableOfContents.vue
+++ app/components/blog/BlogTableOfContents.vue
@@ -4,38 +4,31 @@ import type { TocLink } from "@nuxt/content"
 defineProps<{
   links: TocLink[]
 }>()
-
-const route = useRoute()
 </script>
 
 <template>
   <div>
     <p class="text-lg font-semibold">Table of Contents</p>
-    <ul class="mt-3">
-      <NuxtLink
-        v-for="link in links"
-        :key="link.id"
-        :to="route.path + '#' + link.id"
-      >
-        <li
-          class="hover:text-foreground dark:hover:text-foreground truncate text-sm leading-6 text-neutral-500 dark:text-neutral-500"
+    <ul class="mt-3 space-y-1">
+      <li v-for="link in links" :key="link.id">
+        <NuxtLink
+          class="hover:text-foreground dark:hover:text-foreground block w-full truncate text-sm leading-6 text-neutral-500 dark:text-neutral-500"
+          :href="`#${link.id}`"
         >
-          <span>
-            {{ link.text }}
-          </span>
-          <!--          <NuxtLink-->
-          <!--            v-for="l in link.children"-->
-          <!--            :key="l.id"-->
-          <!--            :to="route.path + '#' + l.id"-->
-          <!--          >-->
-          <!--            <li-->
-          <!--              class="ml-3 truncate text-sm leading-6 text-neutral-500 hover:text-foreground dark:text-neutral-400"-->
-          <!--            >-->
-          <!--              {{ l.text }}-->
-          <!--            </li>-->
-          <!--          </NuxtLink>-->
-        </li>
-      </NuxtLink>
+          {{ link.text }}
+        </NuxtLink>
+
+        <ul v-if="link.children?.length" class="mt-1 space-y-1">
+          <li v-for="child in link.children" :key="child.id">
+            <NuxtLink
+              class="hover:text-foreground dark:hover:text-foreground block w-full truncate pl-3 text-sm leading-6 text-neutral-500 dark:text-neutral-500"
+              :href="`#${child.id}`"
+            >
+              {{ child.text }}
+            </NuxtLink>
+          </li>
+        </ul>
+      </li>
     </ul>
   </div>
 </template>
