e68d79f05984bd7ec941f0b04377d4e9d53a8946
Dino-Kupinic <dinokupinic2006@gmail.com>
2026-02-14 22:12:28 +0100

    feat: enhance copy button functionality with toast notification on success

--- app/components/blog/BlogCopyButton.vue
+++ app/components/blog/BlogCopyButton.vue
@@ -1,18 +1,25 @@
 <script setup lang="ts">
+import { toast } from "vue-sonner"
+
 const props = defineProps<{
   link: string
 }>()
 
-const source = ref<string>(`https://dino-kupinic.dev${props.link}`)
+const { siteUrl } = useSiteRuntimeConfig()
+
+const source = ref<string>(`${siteUrl}${props.link}`)
 const { copy } = useClipboard({ source })
 
-function copyToClipboard() {
-  copy(source.value)
+async function copyToClipboard() {
+  await copy(source.value)
+  toast.success("Copied to clipboard", {
+    position: isMobile.value ? "top-center" : "bottom-right",
+  })
 }
 </script>
 
 <template>
-  <Button variant="ghost" size="default" @click="copyToClipboard()">
+  <Button variant="ghost" size="default" type="button" @click="copyToClipboard">
     <span class="flex items-center gap-1.5 text-sm">
       <Icon name="i-ph-link" size="20" />
       <span>Copy URL</span>
